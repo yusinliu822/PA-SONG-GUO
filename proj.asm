1d091a7db512221bdaa5e573d68f9aa0be8fe443
yusinliu822 <73052719+yusinliu822@users.noreply.github.com>
2021-12-25 23:30:04 +0800

    fix: read input obtuse

--- proj.asm
+++ proj.asm
@@ -3,14 +3,12 @@ INCLUDE page.inc
 main EQU start@0
 
 .data
-    startTime DWORD ?
-    curTime DWORD ?
+    prevTime DWORD 0
     timeLeft DWORD 30
     count DWORD 0
     ranNum DWORD ?
     score DWORD 0
     timeDisplayMsg BYTE "TIME: ",0
-    inputKeyAscii WORD ?
 
 .code
 main PROC
@@ -26,44 +24,28 @@ GeneratePinesPos:
     call PrintPineRows
 
     call GetMseconds
-    mov startTime, eax
+    mov prevTime, eax
     INVOKE Println, OFFSET timeDisplayMsg
-GAMESTART:
-    call GetMseconds
-    mov curTime, eax
-    sub eax, startTime
-    cmp eax, 1000                   ; if duration < 1 sec
-    jl ENDLABEL                     ; then goto ENDLABEL
-ONESECOND:
-    mov eax, curTime
-    mov startTime, eax              ; update startTime
-    dec timeLeft                    ; timeLeft--
-    mov  dl, 6                      ; column
-    mov  dh, 0                      ; row
-    .IF timeLeft == 9
-        call Gotoxy
-        mov al, '0'
-        call WriteChar
-    .ENDIF
-    .IF timeLeft < 10
-        add dl, 1
-    .ENDIF
-    call Gotoxy
-    mov eax, timeLeft
-    call WriteDec                   ; print timeLeft
-    ; call Crlf
-    jmp ENDLABEL
-ENDLABEL:
-    DetectKeyEvent:
-        call ReadKey
-        jz EndKeyEvent
-        mov inputKeyAscii, ax
-        call HandleKeyboard
-    EndKeyEvent:
 
-    mov eax, timeLeft
-    cmp eax, 0                      ; if timeLeft > 0
-    jne GAMESTART                   ; goto GAMESTART
+GameLoop:
+    DetectKeyEvent:
+        mov eax, 20          ; sleep, to allow OS to time slice
+        call Delay           ; (otherwise, some key presses are lost)
+        call ReadKey
+        jz CheckTime
+        call HandleKeyboard
+    CheckTime:
+        call GetMseconds ; now = eax
+        mov ebx, eax
+        sub eax, prevTime
+    OneSecond:
+        .IF eax >= 1000
+            call UpdateTimer
+        .ENDIF
+    CheckGameOver:
+        mov eax, timeLeft
+        cmp eax, 0                      ; if timeLeft > 0
+        jne GameLoop                   ; goto GameLoop
     
 EndPage:
     call Clrscr
@@ -79,14 +61,31 @@ EndPage:
 
 main ENDP
 
+UpdateTimer PROC USES eax edx
+    dec timeLeft                    ; timeLeft--
+    mov  dl, 6                      ; column
+    mov  dh, 0                      ; row
+    .IF timeLeft == 9
+        call Gotoxy 
+        mov al, '0'
+        call WriteChar
+    .ENDIF
+    .IF timeLeft < 10
+        add dl, 1
+    .ENDIF
+    call Gotoxy
+    mov eax, timeLeft
+    call WriteDec                   ; print timeLeft
+    mov prevTime, ebx
+    ret
+UpdateTimer ENDP
+
 HandleKeyboard PROC USES ebx edx
 ReadPosition:
     call GetPinePosition    ; edx = pinePosition
     mov bl, [edx]           ; bl = cur pine position
 
 ReadUserInput:
-    ; call ReadChar
-    mov ax, inputKeyAscii
     .IF bl == "0"
         .IF ax == 4B00h ; LEFT
             call RandomPineRow     ; create new line pinecone
@@ -96,9 +95,8 @@ ReadUserInput:
             INVOKE PrintWhitePine
             ; jmp ReadUserInput
         .ENDIF
-	.ENDIF
 
-	.IF bl == "1"
+	.ELSEIF bl == "1"
         .IF ax == 5000h ; DOWN
             call RandomPineRow     ; create new line pinecone
             call PrintPineRows
@@ -107,9 +105,8 @@ ReadUserInput:
             INVOKE PrintWhitePine
             ; jmp ReadUserInput
         .ENDIF
-	.ENDIF
 
-	.IF bl == "2"
+	.ELSEIF bl == "2"
         .IF ax == 4D00h ; RIGHT
             call RandomPineRow     ; create new line pinecone
             call PrintPineRows
